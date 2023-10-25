35397f887641a1e1dcd52f2aecca18bcd6af7fce
Angel Schiaffini <angelschiaffini@gmail.com>
2023-10-25 11:05:09 -0600

    Servidor Basico

---
+++ index.js
@@ -0,0 +1,17 @@
+//importar bibliotecas
+
+const express = require("express");
+const app = express();
+//middleware
+app.use(express.json());
+
+//Mala practica
+app.get('/test', (request, response)=>{
+    console.log("Esto no se debe de hacer pero funciona");
+    response.send('<h1>Servidor Funcionando</h1>');
+})
+
+//levantar el server y escuchar peticiones
+app.listen(8080, ()=>{
+    console.log("Sevidor escuchando 👍")
+});
